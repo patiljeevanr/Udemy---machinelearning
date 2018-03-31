9e28c2d8bba6befafba6e3f92f3812a53f361a4b
patiljeevanr <patiljeevanr@gmail.com>
2018-03-31 17:38:11 +0530

    Part 2 Section 4 Completed

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -6,7 +6,7 @@ Created on Sat Mar 31 14:05:01 2018
 @author: patiljeevanr
 """
 
-# Data Preprocessing Template
+# Simple Linear Regression
 
 # Importing the libraries
 import numpy as np
@@ -34,3 +34,22 @@ y_train = sc_y.fit_transform(y_train)"""
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+# Visualinsing the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualinsing the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
